a2ec743c8da6270388c2ec83ebb2f1c0f002cfda
parthnariya <68768212+parthnariya@users.noreply.github.com>
2022-07-25 11:33:01 +0530

    with-react-query-hydrate

---
+++ pages/_app.js
@@ -0,0 +1,21 @@
+import "../styles/globals.css";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { useState } from "react";
+
+function MyApp({ Component, pageProps }) {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+
+      <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
